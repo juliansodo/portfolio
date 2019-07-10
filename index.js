dacf54f8c4bee64fd33d327c51442525a98a35b2
juliansodo <37167492+juliansodo@users.noreply.github.com>
2019-07-10 10:11:42 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+// declaro rutas
+
+const rInicio = {template:'<div class="col-md-9">Inicio</div>'}
+
+
+const router = new VueRouter({
+    rutas:[
+      {
+        path:"/",
+        name:"Inicio",
+        component:"<div class='col-md-9'>Inicio</div>"
+      }
+    ]
+});
+
+
+
+//------------------------------> INICIALIZO VUE
+
+const app = new Vue({
+  router, //SIEMPRE -------------- DEBE SER router:loquesea
+  data(){
+    return{
+      trabajos:[
+        {
+          id:'sistemaBursatil',
+          titulo:'Consultas Online',
+          subtitulo:'Sistema de consultas online para empresa bursatil,',
+          caracteristicas:'; Login para el usuario; Recuperación de clave mediante e-mail; Cambio de clave; Consulta completa de cuenta; Filtro por fechas, especie y cantidad de resultados; Sistema administrativo; Registro de movimientos en la web tanto de los usuarios como de los administradores',
+          tareasUsuario:'El usuario podrá consultar el estado de su cuenta, el saldo, tanto en pesos como en dólares, el usuario podrá ver el estado de sus inversiones en tablas, gráficos. También podrá ver los precios de las especies en cualquier día (sin API); CUENTA CORRIENTE - TENENCIA - HISTÓRICO - RESULTADO - OPERACIONES - PRECIOS ',
+          admin: true,
+          tareasAdministrador:
+          [
+          'Alta, baja y modificación de los usuarios, posibilidad de deshabilitar o bloquear cuenta temporalmente.',
+          'Creación de cuenta con contraseña aleatoria, el nuevo usuario recibirá un e-mail con la contraseña.',
+          'El ceo de la empresa posee una query, la cual sirve para realizar ciertas cosas dentro de la web.',
+          'El administrador tiene un registro de todo lo que sucede en la web, tanto para los usuarios como los administradores.',
+          'Personalización del sitio.',
+          'El administrador puede recibir consultas y leerlas dentro del panel, pudiendo responderlas en el lugar. '
+          ],
+          media:[
+            {
+
+            }
+          ],
+          tecnologias:
+          [
+            "HTML",
+            "CSS (Bootstrap, Semantic ui)",
+            "JS"
+          ]
+        }
+      ]
+    }
+  }
+}).$mount('#app');
---
+++ rutas.js
@@ -0,0 +1,13 @@
+// declaro rutas
+
+const rInicio = {template:'<div class="col-md-9">Inicio</div>'}
+
+
+const rutas = [
+    { path: '/', component: rInicio },
+  ] 
+const ruteo = new VueRouter({
+    rutas
+});
+
+
